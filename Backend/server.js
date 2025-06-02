27118e8153256d2b7fe8b4ae76a4808a21861442
Shudhansu Singh <sunnybst2001@gmail.com>
2025-06-02 10:22:21 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
     console.log('Server is running on http://127.0.0.1:3000/');
 }) 
