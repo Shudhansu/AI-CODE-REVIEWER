cde083d4e707fd8f7fd6d59bb3d6ccdc290ddb65
Shudhansu Singh <sunnybst2001@gmail.com>
2025-06-02 10:39:55 +0530

    Third commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://127.0.0.1:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-reviewer-backend-99i9.onrender.com', { code })
     setReview(response.data)
   }
 
